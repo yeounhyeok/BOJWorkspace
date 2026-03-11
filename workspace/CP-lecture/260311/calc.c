08b999d6370ce1ebbf684ef6d76deb061d04d365
yeounhyeok <yeopeter1031@naver.com>
2026-03-11 14:38:11 +0000

    First commit on dev container! - 260311

---
+++ workspace/CP-lecture/260311/calc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %d\n", a, b, a/b);
+
+    return 0;
+}
+/*
+13.192.194.33/32
+35.72.131.211/32
+*/
---
+++ workspace/CP-lecture/260311/print.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    char university[20] = "한성대학교";
+    char department[20] = "AI응용학과";
+    char name[10] = "여운혁";
+    int st_number = 2393008;
+
+    printf("------------------\n");
+    printf("학교 : %s\n", university);
+    printf("학과 : %s\n", department);
+    printf("이름 : %s\n", name);
+    printf("학번 : %d\n", st_number);
+    printf("------------------\n");
+
+    return 0;
+}
